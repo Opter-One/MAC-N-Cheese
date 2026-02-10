9c456546dff89061b48aa9e2639a964afe9fb73a
Opter-One <luca.colombo07@universitadipavia.it>
2026-02-10 20:05:37 +0100

    First try

--- src/project.v
+++ src/project.v
@@ -1,11 +1,11 @@
 /*
- * Copyright (c) 2024 Your Name
+ * Copyright (c) 2026 Luca Colombo
  * SPDX-License-Identifier: Apache-2.0
  */
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_mac_n_cheese (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -16,12 +16,38 @@ module tt_um_example (
     input  wire       rst_n     // reset_n - low to reset
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
   assign uio_out = 0;
   assign uio_oe  = 0;
-
+  
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire _unused = &{1'b0};
+
+  // Wire declarations for internal logic
+  // 32 bits accumulatore to hold the result of the multiplication
+  reg [31:0] accumulator = 0, sample_o;
+  reg [3:0] counter = 0; // Counter to keep track of the number of multiplications
+
+  always @(posedge clk or negedge rst_n) begin
+    if(~rst_n) begin
+      accumulator <= 0;
+      sample_o <= 0;
+      counter <= 0;
+    end else if (ena) begin
+      // After 8 multiplications, output the result and reset the accumulator and counter
+      if (counter == 7) begin
+        sample_o <= (accumulator + (ui_in * uio_in)); // Output the least significant byte of the result
+        accumulator <= 0; // Reset the accumulator for the next round of multiplications
+        counter <= 0; // Reset the counter
+      end else begin
+        sample_o <= 0; // Clear the output until we have a valid result
+        // Perform multiplication and accumulate the result
+        accumulator <= accumulator + (ui_in * uio_in);
+        // Increment the counter
+        counter <= counter + 1;
+      end
+    end
+  end
+
+  assign uo_out = sample_o[31:24]; // Output the most significant byte of the accumulated result
 
 endmodule
